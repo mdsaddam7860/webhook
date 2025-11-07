e9967e298aa8a7d0dc00ea8518015ee4448c4c66
Md Saddam <mdsonu7860@outlook.com>
2025-11-07 15:34:47 +0530

    Update webhook.js to log message when app is running

--- webhook.js
+++ webhook.js
@@ -13,7 +13,8 @@ app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true, limit: "5mb" }));
 
 app.get("/", (req, res) => {
-  res.send("<h1>🚀 App is running</h1>");
+  logMessage("App is running");
+  res.send("<h1>App is running</h1>");
 });
 
 // ✅ Webhook endpoint
